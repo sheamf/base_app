8278d1bae0facfa01089d414158000f58009b72d
sheamf <miles_shea@yahoo.com>
2013-10-14 07:12:23 -0600

    add static_pages and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 BaseApp::Application.routes.draw do
+
   devise_for :users
+
+	resources :users do
+	end
+
+	root to: 'static_pages#home'
+	match '/', 					to: 'static_pages#home'
+	match 'about',			to: 'static_pages#about'
+	match 'contact',		to: 'static_pages#contact'
+	
+
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
